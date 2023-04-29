c42b7e9246a4853df388b0a3df272df9be5005ea
oreofyok <jitpipat2539yok@gmail.com>
2023-04-29 13:37:27 +0700

    eq

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+
+        Calculator calculator1 = new Calculator("f",2);
+        System.out.println((calculator1.equal));
+
+    }
+}
